7da2aa7c3d0eacffec7fa50915fdc4f49f5425f4
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-14 09:40:23 +0530

    challenge done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState();
+  const [heading, setHeading] = useState();
+
+  function handelChange(even) {
+    setName(even.target.value);
+  }
+  function SubmitBtn() {
+    setHeading(name);
+  }
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {heading}</h1>
+      <input
+        onChange={handelChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={SubmitBtn}>Submit</button>
     </div>
   );
 }
